69021c20b3fa63a37e4b6f0edefa7313997b62ac
Javier Corral <javier.corral360@gmail.com>
2024-06-27 20:57:28 -0700

    added javier

--- J_PDF_Merge/J_PDF_Merge/obj/Debug/net8.0/J_PDF_Merge.AssemblyInfo.cs
+++ J_PDF_Merge/J_PDF_Merge/obj/Debug/net8.0/J_PDF_Merge.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("J_PDF_Merge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e73d0fbc167ab2d44b2d1f0c0e4d9e0907ff12dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("J_PDF_Merge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("J_PDF_Merge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
